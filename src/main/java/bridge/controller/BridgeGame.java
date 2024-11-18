8ba7f848c732f9e6cb6f7cdafa8a1fb6a1f8319e
Teddysir <sdzx0719@naver.com>
2024-11-18 17:52:57 +0900

    feat: 다리 건너기 결과 출력 메서드 추가

--- src/main/java/bridge/controller/BridgeGame.java
+++ src/main/java/bridge/controller/BridgeGame.java
@@ -2,8 +2,11 @@ package bridge.controller;
 
 import bridge.BridgeNumberGenerator;
 import bridge.BridgeRandomNumberGenerator;
+import bridge.model.BridgeChecker;
 import bridge.model.BridgeMaker;
 import bridge.view.InputView;
+
+import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -11,21 +14,38 @@ import bridge.view.InputView;
 public class BridgeGame {
 
     private final InputView inputView = new InputView();
+    private final BridgeChecker bridgeChecker = new BridgeChecker();
     private final BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
     private final BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
 
     public void start() {
-        setUp();
-        move();
+        bridgeCheck();
         retry();
     }
 
-    public void setUp() {
+    public void bridgeCheck() {
         inputView.guidGameStartMessage();
         String rawBridgeSizeInput = inputView.guideBridgeSizeInput();
         int bridgeSize = inputView.readBridgeSize(rawBridgeSizeInput);
 
-        bridgeMaker.makeBridge(bridgeSize);
+        List<String> answerBridge = bridgeMaker.makeBridge(bridgeSize);
+
+        game(answerBridge);
+    }
+
+    public void game(List<String> answerBridge) {
+        System.out.println(answerBridge);
+        StringBuilder answerSbUp = new StringBuilder();
+        StringBuilder answerSbDown = new StringBuilder();
+        answerSbUp.append("[");
+        answerSbDown.append("[");
+        for (int checkIndex = 0; checkIndex < answerBridge.size(); checkIndex++) {
+            String movingCommand = move();
+            answerSbUp.append(bridgeChecker.guessUpBridge(answerBridge, movingCommand, checkIndex));
+            answerSbDown.append(bridgeChecker.guessDownBridge(answerBridge, movingCommand, checkIndex));
+            System.out.println(answerSbUp);
+            System.out.println(answerSbDown);
+        }
     }
 
     /**
@@ -33,9 +53,9 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public String move() {
         inputView.guideMovingCommand();
-        String movingCommand = inputView.readMoving();
+        return inputView.readMoving();
     }
 
     /**
---
+++ src/main/java/bridge/model/BridgeChecker.java
@@ -0,0 +1,99 @@
+package bridge.model;
+
+import java.util.List;
+
+public class BridgeChecker {
+
+
+    public StringBuilder guessUpBridge(List<String> answerBridge, String movingCommand, int checkIndex) {
+
+        String answer = answerBridge.get(checkIndex);
+        StringBuilder sbUp = new StringBuilder();
+
+        if (answer.equals("D")) {
+            if (answerBridge.size() - 1 == checkIndex) {
+                if (answer.equals(movingCommand)) {
+                    sbUp.append("   ]");
+                }
+                if (!answer.equals(movingCommand)) {
+                    sbUp.append("   ]");
+                }
+                return sbUp;
+            }
+            if (!answer.equals(movingCommand)){
+                sbUp.append(" X |");
+                return sbUp;
+            }
+            sbUp.append("   |");
+        }
+
+        if (answer.equals("U")) { // 정답이 U임
+            if (answerBridge.size() - 1 == checkIndex) {
+                if (answer.equals(movingCommand)) {
+                    sbUp.append(" O ]");
+                }
+                if (!answer.equals(movingCommand)) {
+                    sbUp.append("   ]");
+                }
+                return sbUp;
+            }
+            if (answer.equals(movingCommand)) { // 올바른 정답을 입력할 경우
+                sbUp.append(" O |");
+            }
+            if (!answer.equals(movingCommand)) {
+                sbUp.append("   |");
+            }
+        }
+
+        return sbUp;
+
+    }
+
+    public StringBuilder guessDownBridge(List<String> answerBridge, String movingCommand, int checkIndex) {
+
+        String answer = answerBridge.get(checkIndex);
+        StringBuilder sbDown = new StringBuilder();
+
+        if (answer.equals("U")) {
+
+            if (answerBridge.size() - 1 == checkIndex) {
+                if (answer.equals(movingCommand)) {
+                    sbDown.append("   ]");
+                }
+                if (!answer.equals(movingCommand)) {
+                    sbDown.append("   ]");
+                }
+                return sbDown;
+            }
+            if (!answer.equals(movingCommand)){
+                sbDown.append(" X |");
+                return sbDown;
+            }
+            sbDown.append("   |");
+        }
+
+        if (answer.equals("D")) {
+            if (answerBridge.size() - 1 == checkIndex) {
+                if (answer.equals(movingCommand)) {
+                    sbDown.append(" O ]");
+                }
+                if (!answer.equals(movingCommand)) {
+                    sbDown.append("   ]");
+                }
+                return sbDown;
+            }
+
+            if (answer.equals(movingCommand)) {
+                sbDown.append(" O |");
+            }
+            if (!answer.equals(movingCommand)) {
+                sbDown.append("   |");
+            }
+        }
+
+        return sbDown;
+
+    }
+
+
+}
