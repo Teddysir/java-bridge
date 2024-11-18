89259f401b5fb4232ef25465e19a3455a574ef44
Teddysir <sdzx0719@naver.com>
2024-11-18 15:07:03 +0900

    feat: 다리 길이 형식 유효성 검사 메서드 추가

--- src/main/java/bridge/constants/ErrorMessageType.java
+++ src/main/java/bridge/constants/ErrorMessageType.java
@@ -2,7 +2,8 @@ package bridge.constants;
 
 public enum ErrorMessageType {
 
-    ERROR_BRIDGE_LENGTH("[ERROR] 다리 길이는 3~20 사이의 숫자만 가능합니다. 다시 입력 해주세요.");
+    ERROR_BRIDGE_LENGTH("[ERROR] 다리 길이는 3~20 사이의 숫자만 가능합니다. 다시 입력 해주세요."),
+    ERROR_INVALID_BRIDGE_LENGTH_FORMAT("[ERROR] 올바르지 않은 다리 길이 형식입니다. 다시 입력 해주세요.");
 
     private final String message;
 
--- src/main/java/bridge/utils/GameValidator.java
+++ src/main/java/bridge/utils/GameValidator.java
@@ -2,11 +2,21 @@ package bridge.utils;
 
 import bridge.constants.ErrorMessageType;
 
+import java.util.IllegalFormatException;
+
 public class GameValidator {
 
     public static void checkedBridgeLength(int bridgeLength) {
         if (bridgeLength < 3 || bridgeLength > 20) {
             throw new IllegalArgumentException(ErrorMessageType.ERROR_BRIDGE_LENGTH.getMessage());
+        }
+    }
+
+    public static int checkedBridgeLengthFormat(String rawBridgeLength) {
+        try {
+            return Integer.parseInt(rawBridgeLength);
+        } catch (IllegalFormatException e) {
+            throw new IllegalArgumentException(ErrorMessageType.ERROR_INVALID_BRIDGE_LENGTH_FORMAT.getMessage());
         }
     }
 
